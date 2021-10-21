d91055d76095b59e99adb3c1190e90535f004150
Ielliena <ielliena122@gmail.com>
2021-10-21 16:39:07 +0500

    Impove somw bugs

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,12 @@
 	<div id="app">
 		<Title @startGame="startGame" :isStarted="isStarted" />
 		<div class="container">
-			<Grid v-if="isStarted"
-				@cellClick="cellClick"  
-				:board="board"
-			/>
-			<p class="gameNotStart" v-else>{{winnerText}}</p>
-			<Panel 
-				:currentPlayer="currentPlayer" 
-				:moveCounter="moveCounter" 
-				:score="score" 
+			<Grid v-if="isStarted" @cellClick="cellClick" :board="board" />
+			<p class="gameNotStart" v-else>{{ winnerText }}</p>
+			<Panel
+				:currentPlayer="currentPlayer"
+				:moveCounter="moveCounter"
+				:score="score"
 			/>
 		</div>
 	</div>
@@ -31,14 +28,14 @@ export default {
 	data: function() {
 		return {
 			winConditions: [
-				[0,1,2],
-				[3,4,5],
-				[6,7,8],
-				[0,3,6],
-				[1,4,7],
-				[2,5,8],
-				[0,4,8],
-				[2,4,6],
+				[0, 1, 2],
+				[3, 4, 5],
+				[6, 7, 8],
+				[0, 3, 6],
+				[1, 4, 7],
+				[2, 5, 8],
+				[0, 4, 8],
+				[2, 4, 6],
 			],
 			isStarted: false,
 			winnerText: "Нажмите СТАРТ для начала игры",
@@ -47,43 +44,53 @@ export default {
 			moveCounter: 0,
 			score: {
 				player1: 0,
-				player2: 0
-			}
+				player2: 0,
+			},
 		};
 	},
 	methods: {
 		startGame() {
-			this.isStarted = !this.isStarted;
+			const started = this.isStarted;
+			this.nullData();
+			this.isStarted = !started;
 		},
 		cellClick(elem) {
 			const cell = elem.getAttribute("data-cell-index");
-			if(this.board[cell] != "") return;
+			if (this.board[cell] != "") return;
 
 			elem.classList.add("cell" + this.currentPlayer);
 			this.board[cell] = this.currentPlayer;
-			this.checkWinners();
+
+			if (!this.checkWinners() && this.moveCounter == 9) {
+				this.nullData();
+				this.winnerText = "Нет победителей";
+			}
+		},
+		nullData() {
+			this.isStarted = false;
+			this.board = ["", "", "", "", "", "", "", "", ""];
+			this.currentPlayer = "X";
+			this.moveCounter = 0;
 		},
 		checkWinners() {
-			for(let i = 0; i < 7; ++i) {
+			for (let i = 0; i < 7; ++i) {
 				const winCondition = this.winConditions[i];
-				if(
+				if (
 					this.board[winCondition[0]] != "" &&
 					this.board[winCondition[0]] == this.board[winCondition[1]] &&
 					this.board[winCondition[1]] == this.board[winCondition[2]]
 				) {
-					this.isStarted = false;
-					this.moveCounter = 0;
-					this.board = ["", "", "", "", "", "", "", "", ""];
 					if (this.currentPlayer == "X") ++this.score.player1;
 					else ++this.score.player2;
 					this.winnerText = "Победитель - игрок " + this.currentPlayer;
-					this.currentPlayer = "X";
-					return;
+					this.nullData();
+					return true;
 				}
 			}
 			this.currentPlayer = this.currentPlayer == "X" ? "O" : "X";
-			++(this.moveCounter);
-		}
+			++this.moveCounter;
+			return false;
+		},
 	},
 };
 </script>
